adeb05dbc161047c4cf83d0369604560d20079c7
Richard Kweku Aikins <richardkns7@gmail.com>
2025-03-29 07:08:48 +0000

    Update ServerProviderResource.php

--- app/Filament/Resources/ServerProviderResource.php
+++ app/Filament/Resources/ServerProviderResource.php
@@ -17,24 +17,14 @@ class ServerProviderResource extends Resource
 {
     protected static ?string $model = ServerProvider::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static bool $shouldRegisterNavigation = false;
+    
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('slug')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\Textarea::make('description')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\FileUpload::make('image')
-                    ->required()
-                    ->maxLength(255),
+               
             ]);
     }
 
@@ -42,17 +32,20 @@ class ServerProviderResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\ImageColumn::make('image'),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('select')->label('Select')->url(fn (ServerProvider $record) => route('filament.admin.resources.server-providers.edit', $record->id)),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    // Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
