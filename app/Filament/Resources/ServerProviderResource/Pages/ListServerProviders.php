66b720e933a1074fcd5dfb903e096d0120309c94
Richard Kweku Aikins <richardkns7@gmail.com>
2025-03-29 06:50:05 +0000

    Add ListServerProviders.php

---
+++ app/Filament/Resources/ServerProviderResource/Pages/ListServerProviders.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ServerProviderResource\Pages;
+
+use App\Filament\Resources\ServerProviderResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListServerProviders extends ListRecords
+{
+    protected static string $resource = ServerProviderResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
