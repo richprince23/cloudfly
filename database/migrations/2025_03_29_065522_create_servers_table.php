1c308611c30c9ca3ebf435bc970d39ddccbd2aa3
Richard Kweku Aikins <richardkns7@gmail.com>
2025-03-29 07:08:49 +0000

    Add 2025_03_29_065522_create_servers_table.php

---
+++ database/migrations/2025_03_29_065522_create_servers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('server_provider_id')->constrained('server_providers');
+            $table->string('name');
+            $table->string('access_key')->nullable();
+            $table->string('api_key')->nullable();
+            $table->string('api_url')->nullable();
+            $table->string('api_version')->nullable();
+            $table->string('api_endpoint')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servers');
+    }
+};
