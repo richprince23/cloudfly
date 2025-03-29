d3e621cf489b8ffcd35c32817a8b2a5245054a94
Richard Kweku Aikins <richardkns7@gmail.com>
2025-03-29 09:15:38 +0000

    Update Region.php

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -7,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class Region extends Model
 {
     //
+
+
+    public function machines()
+    {
+        return $this->hasMany(Machine::class);
+    }
+
+    public function serverProvider()
+    {
+        return $this->belongsTo(ServerProvider::class);
+    }
+
+    public function server()
+    {
+        return $this->belongsTo(Server::class);
+    }
+
+    public function environment()
+    {
+        return $this->belongsTo(Environment::class);
+    }
+
+    
 }
