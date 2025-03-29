e0d71492b2a0c7af597e304fd5c3ff7607c8ed18
Richard Kweku Aikins <richardkns7@gmail.com>
2025-03-29 08:04:36 +0000

    Update Server.php

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Server extends Model
 {
-    //
+    protected $fillable = [
+        'server_provider_id',
+        'name',
+        'access_key',
+        'api_key',
+        'api_endpoint',
+        'ssh_ip_address',
+        'ssh_port',
+        'ssh_public_key',
+    ];
+
+    public function serverProvider()
+    {
+        return $this->belongsTo(ServerProvider::class);
+    }
 }
