19759d7a34fa923b8266a28782f93a9ab92fb8b3
Richard Kweku Aikins <richardkns7@gmail.com>
2025-03-29 06:50:04 +0000

    Add CreateServerProvider.php

---
+++ app/Filament/Resources/ServerProviderResource/Pages/CreateServerProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ServerProviderResource\Pages;
+
+use App\Filament\Resources\ServerProviderResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateServerProvider extends CreateRecord
+{
+    protected static string $resource = ServerProviderResource::class;
+}
